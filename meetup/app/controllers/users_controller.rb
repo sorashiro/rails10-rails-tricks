94815b8c55294cd173c7fc14973c150a1896c758
yfwang <yufei930101@gmail.com>
2015-07-20 16:23:15 +0800

    add has_secure_password

--- meetup/app/controllers/users_controller.rb
+++ meetup/app/controllers/users_controller.rb
@@ -2,4 +2,15 @@ class UsersController < ApplicationController
   def signup
     @user = User.new
   end
+
+  def create
+    user = User.new(user_params)
+    user.save
+    redirect_to :root
+  end
+
+  private
+  def user_params
+    params.require(:user).permit!
+  end
 end
