243c0d1a601f9873d253647dcd99274a872497bb
yfwang <yufei930101@gmail.com>
2015-07-21 09:15:56 +0800

    add session to login

--- meetup/app/controllers/users_controller.rb
+++ meetup/app/controllers/users_controller.rb
@@ -12,6 +12,7 @@ class UsersController < ApplicationController
   def create_login_session
     user = User.find_by_name(params[:name])
     if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
       redirect_to :root
     else
       redirect_to :login
