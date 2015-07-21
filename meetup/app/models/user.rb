7b7dd821f59f618676707367b73035b9df1907d5
yfwang <yufei930101@gmail.com>
2015-07-21 13:40:07 +0800

    add 表单验证

--- meetup/app/models/user.rb
+++ meetup/app/models/user.rb
@@ -1,6 +1,8 @@
 class User < ActiveRecord::Base
   has_secure_password
   before_create { generate_token(:auth_token) }
+  validates :name, :email, presence: true
+  validates :name, :email, uniqueness: { case_sensitive: false }
 
   def generate_token(column)
     begin
