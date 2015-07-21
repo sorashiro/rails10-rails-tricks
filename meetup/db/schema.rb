0dfddc6f8b3889c5fe1ab203070fd1ff0a4baa8d
yfwang <yufei930101@gmail.com>
2015-07-21 11:43:59 +0800

    add auth-token to users

--- meetup/db/schema.rb
+++ meetup/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150720072313) do
+ActiveRecord::Schema.define(version: 20150721033729) do
 
   create_table "comments", force: true do |t|
     t.text     "content"
@@ -35,6 +35,7 @@ ActiveRecord::Schema.define(version: 20150720072313) do
     t.string   "password_digest"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "auth_token"
   end
 
 end
